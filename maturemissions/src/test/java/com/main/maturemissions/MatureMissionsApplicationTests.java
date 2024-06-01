fdc4377ebdf7937a0c5e4160420901a404a96b13
Raghav Ravisankar <rrav2109@uni.sydney.edu.au>
2024-06-01 15:56:18 +1000

    Added changes to get tests working when deploying

--- maturemissions/src/test/java/com/main/maturemissions/MatureMissionsApplicationTests.java
+++ maturemissions/src/test/java/com/main/maturemissions/MatureMissionsApplicationTests.java
@@ -2,8 +2,10 @@ package com.main.maturemissions;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class MatureMissionsApplicationTests {
 
 	@Test
